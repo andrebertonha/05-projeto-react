0e6a332ce867dde64da63cfdb052821eba71f7a5
andre <andrebertonhadev@gmail.com>
2020-12-30 12:48:41 -0800

    div style css

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -13,6 +14,18 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do Repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img src="https://avatars0.githubusercontent.com/u/15649530?s=460&u=d3b258fc4cf152c6b2aa6f7511b6ad07372a9d9f&v=4" alt="André Martins"/>        
+          <div>
+            <strong>/andrebertonha/05-projeto-react</strong>
+            <p>gostack 12 study gostack 12 study gostack 12 study vgostack 12 study</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   ) 
 };
