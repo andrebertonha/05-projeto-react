2b1bbf11a3e2891902b4052b0e52b87b955dfae7
andre <andrebertonhadev@gmail.com>
2021-01-05 06:39:37 -0800

    eslint and prettier config for react pŕoject

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,37 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg';
 import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  function handleAddRepository() {
+
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore Repositórios no github;</Title>
 
       <Form>
-        <input placeholder="Digite o nome do Repositório" />
+        <input 
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do Repositório" 
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/15649530?s=460&u=d3b258fc4cf152c6b2aa6f7511b6ad07372a9d9f&v=4" alt="André Martins"/>        
-          <div>
-            <strong>/andrebertonha/05-projeto-react</strong>
-            <p>gostack 12 study gostack 12 study gostack 12 study vgostack 12 study</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img src="https://avatars0.githubusercontent.com/u/15649530?s=460&u=d3b258fc4cf152c6b2aa6f7511b6ad07372a9d9f&v=4" alt="André Martins"/>        
-          <div>
-            <strong>/andrebertonha/05-projeto-react</strong>
-            <p>gostack 12 study gostack 12 study gostack 12 study vgostack 12 study</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
         <a href="teste">
           <img src="https://avatars0.githubusercontent.com/u/15649530?s=460&u=d3b258fc4cf152c6b2aa6f7511b6ad07372a9d9f&v=4" alt="André Martins"/>        
           <div>
